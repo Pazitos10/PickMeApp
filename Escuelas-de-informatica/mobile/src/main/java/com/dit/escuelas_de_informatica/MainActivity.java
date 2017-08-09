4bb57c1238ce8cf86ad2375a5cfcfce927e52179
LeoMorales <leo.moralesr23@gmail.com>
2017-08-09 19:28:41 -0300

    start a bosquejo de la app wear we

--- Escuelas-de-informatica/mobile/src/main/java/com/dit/escuelas_de_informatica/MainActivity.java
+++ Escuelas-de-informatica/mobile/src/main/java/com/dit/escuelas_de_informatica/MainActivity.java
@@ -3,41 +3,116 @@ package com.dit.escuelas_de_informatica;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        MessageApi.MessageListener {
 
     private MainFragment mMainFragment;
-    private FloatingActionButton mBotonFlotante;
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "MainActivity";
+    private boolean mResolvingError = false;
+    //Request code for launching the Intent to resolve Google Play services errors.
+    private static final int REQUEST_RESOLVE_ERROR = 1000;
+    private static final String START_ACTIVITY_PATH = "/start-wearapp";
+
     private String[] mListaContenido = new String[]{"Lugar 1", "Lugar 2"};
     private int mFragmentActivo;
     private Toolbar mToolbar;
+
+    /* views declarations: */
+    private BottomNavigationView navigation;
+    private FloatingActionButton mBotonFlotante;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
+        inicializar();
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+
+    }
+
+    /**
+     * Lifecycle
+     *       onStart
+     *       onResume
+     *       onPause
+     *       onStop
+     **/
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        if (!mResolvingError && (mGoogleApiClient != null) && (mGoogleApiClient.isConnected())) {
+            //Wearable.DataApi.removeListener(mGoogleApiClient, this);
+            Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    private void setupViews() {
         mToolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(mToolbar);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         mBotonFlotante = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         mFragmentActivo = R.id.navigation_places;
         mBotonFlotante.setOnClickListener(this.onBotonCliqueado());
-        inicializar();
 
     }
 
@@ -109,6 +184,115 @@ public class MainActivity extends AppCompatActivity {
         transaction.add(R.id.frame_content, mMainFragment, "mainFragment");
         transaction.commit();
 
+    }
+
+    /**
+     * MessageApi.MessageListener callback
+     */
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        LOGD(TAG, "onMessageReceived() A message from watch was received:"
+                + messageEvent.getRequestId() + " " + messageEvent.getPath());
+
+    }
+
+    /**
+     * Sends an RPC to start a fullscreen Activity on the wearable.
+     */
+    public void onStartWearableActivityClick(android.view.MenuItem ignore) {
+        LOGD(TAG, "Generating RPC");
+        Toast.makeText(MainActivity.this, "Up wear!", Toast.LENGTH_SHORT).show();
+
+        // Trigger an AsyncTask that will query for a list of connected nodes and send a
+        // "start-activity" message to each connected node.
+        new StartWearableActivityTask().execute();
+    }
+
+    private void sendStartActivityMessage(String node) {
+        Wearable.MessageApi.sendMessage(
+                mGoogleApiClient, node, START_ACTIVITY_PATH, new byte[0]).setResultCallback(
+                new ResultCallback<MessageApi.SendMessageResult>() {
+                    @Override
+                    public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                        if (!sendMessageResult.getStatus().isSuccess()) {
+                            Log.e(TAG, "Failed to send message with status code: "
+                                    + sendMessageResult.getStatus().getStatusCode());
+                        }
+                    }
+                }
+        );
+    }
+
+
+
+    private class StartWearableActivityTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... args) {
+            Collection<String> nodes = getNodes();
+            for (String node : nodes) {
+                sendStartActivityMessage(node);
+            }
+            return null;
+        }
+    }
+
+    private Collection<String> getNodes() {
+        HashSet<String> results = new HashSet<>();
+        NodeApi.GetConnectedNodesResult nodes =
+                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+
+        for (Node node : nodes.getNodes()) {
+            results.add(node.getId());
+        }
+
+        return results;
+    }
+
+
+    /**
+     * GoogleApiClient buil callbacks:
+     *      onConnected (ConnectionCallbacks)
+     *      onConnectionSuspended (ConnectionCallbacks)
+     *      onConnectionFailed (OnConnectionFailedListener)
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        LOGD(TAG, "Google API Client was connected");
+        mResolvingError = false;
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        LOGD(TAG, "Connection to Google API client was suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!mResolvingError) {
+
+            if (result.hasResolution()) {
+                try {
+                    mResolvingError = true;
+                    result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+                } catch (IntentSender.SendIntentException e) {
+                    // There was an error with the resolution intent. Try again.
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                Log.e(TAG, "Connection to Google API client has failed");
+                mResolvingError = false;
+            }
+        }
+    }
+
+    /**
+     * As simple wrapper around Log.d
+     */
+    private static void LOGD(final String tag, String message) {
+        if (Log.isLoggable(tag, Log.DEBUG)) {
+            Log.d(tag, message);
+        }
     }
 
 
